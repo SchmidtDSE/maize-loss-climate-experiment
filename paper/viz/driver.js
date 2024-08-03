1abe6bf9ae51f32be713e236f4611a8599bbb960
Sam Pottinger <sam.pottinger@berkeley.edu>
2024-08-02 17:37:13 -0700

    Add accessibility options for symbols and emojis.

--- paper/viz/driver.js
+++ paper/viz/driver.js
@@ -35,26 +35,14 @@ function openWebApp(event) {
 }
 
 
-function main() {
-    tippy("[data-tippy-content]");
-
+function initTabs() {
     const tabs = new Tabby("[data-tabs]");
-    const slider = tns({
-        container: ".intro-slider",
-        items: 1,
-        slideBy: "page",
-        autoplay: false,
-        nav: false
-    });
 
     document.addEventListener("tabby", function (event) {
         const url = new URL(event.target.href);
         window.location.hash = url.hash;
         window.scrollTo(0, 0);
     }, false);
-
-    const loadLinks = document.querySelectorAll(".load-app-link");
-    loadLinks.forEach((x) => x.addEventListener("click", openWebApp));
 
     const tabAdvanceLinks = document.querySelectorAll(".advance-button");
     tabAdvanceLinks.forEach((x) => x.addEventListener("click", (event) => {
@@ -64,6 +52,17 @@ function main() {
         window.scrollTo(0, 0);
         event.preventDefault();
     }));
+}
+
+
+function initSlider() {
+    const slider = tns({
+        container: ".intro-slider",
+        items: 1,
+        slideBy: "page",
+        autoplay: false,
+        nav: false
+    });
 
     const slideAdvanceLinks = document.querySelectorAll(".slide-advance-link");
     slideAdvanceLinks.forEach((x) => x.addEventListener("click", (event) => {
@@ -85,12 +84,54 @@ function main() {
         document.getElementById("#intro-slider").scrollIntoView({"behavior": "smooth", "block": "start"});
         document.getElementById("finish-slide").focus();
     });
-    
+}
+
+
+function initInteractivesLinks() {
+    const loadLinks = document.querySelectorAll(".load-app-link");
+    loadLinks.forEach((x) => x.addEventListener("click", openWebApp));
+}
+
+
+function initBespokeControls() {
     document.getElementById("expand-rate-var-link").addEventListener("click", (event) => {
         document.getElementById("rate-var-details").setAttribute("open", true);
         document.getElementById("rate-var-details").focus();
         event.preventDefault();
     });
+}
+
+
+function initAccessibility() {
+    const symbolRadios = document.querySelectorAll(".symbols-setting-radio");
+    symbolRadios.forEach((x) => x.addEventListener("change", function () {
+        if (this.checked) {
+            const emojis = document.querySelectorAll(".emoji");
+            const chverons = document.querySelectorAll(".chevron");
+
+            const hide = (x) => x.style.display = "none";
+            const show = (x) => x.style.display = "inline-block";
+
+            if (this.value === "hide") {
+                emojis.forEach(hide);
+                chverons.forEach(hide);
+            } else {
+                emojis.forEach(show);
+                chverons.forEach(show);
+            }
+        }
+    }));
+}
+
+
+function main() {
+    tippy("[data-tippy-content]");
+
+    initTabs();
+    initSlider();
+    initInteractivesLinks();
+    initBespokeControls();
+    initAccessibility();
 }
 
 
