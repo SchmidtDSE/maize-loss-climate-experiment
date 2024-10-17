90cc0ae422953e7bef36f04fc79bacdf44bc05a6
Sam Pottinger <sam.pottinger@berkeley.edu>
2024-10-17 23:34:16 +0000

    Add package.sh to make instructions simplier.

--- package.sh
+++ package.sh
@@ -10,11 +10,25 @@ cp workspace/export_combined_tasks.csv package/outputs/export_combined_tasks.csv
 cp workspace/export_climate.csv package/outputs/export_climate.csv
 
 mkdir package/data
-cp -r workspace/census_states package/data/census_states
-cp workspace/sim_hist.csv package/data/sim_hist.csv
-cp workspace/sim_ag_all.csv package/data/sim_ag_all.csv
-cp workspace/tool.csv package/data/tool.csv
+cp workspace/export_hist.csv package/data/sim_hist.csv
+cp workspace/export_sweep.csv package/data/sim_ag_all.csv
+cp workspace/export_summary.csv package/data/tool.csv
 cp workspace/export_claims.csv package/data/export_claims.csv
-cp workspace/climate.csv package/data/climate.csv
-cp workspace/states.json.geojson package/data/states.json.geojson
-cp workspace/states.json.qmd package/data/states.json.qmd
+cp workspace/export_climate.csv package/data/climate.csv
+
+rm -r package/build
+mkdir package/build
+cd package/build
+wget https://ag-adaptation-study.pub/archive/data.zip
+unzip data.zip
+cd ../..
+
+cp -r package/build/data/census_states package/data/census_states
+cp package/build/data/states.json.geojson package/data/states.json.geojson
+cp package/build/data/states.json.qmd package/data/states.json.qmd
+
+cd package
+rm data.zip
+rm outputs.zip
+zip data.zip -r data
+zip outputs.zip -r outputs
