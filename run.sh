d75949fab5f93b8091582f2c8830d5d507c7763d
A Samuel Pottinger <sam@gleap.org>
2024-11-26 22:56:09 +0000

    Move initial cluster to preprocess.

--- run.sh
+++ run.sh
@@ -1,4 +1,4 @@
 mkdir workspace
 
-python3 -m luigi --module breakpoint_tasks FullClimatePreprocessTask --local-scheduler --workers 8
+python3 -m luigi --module breakpoint_tasks RunThroughPreprocessTask --local-scheduler --workers 8
 python3 -m luigi --module breakpoint_tasks ExecuteAllWithCluster --local-scheduler --workers 4
