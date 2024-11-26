28356b7d4c08d0417044959ba95f921250407a88
A Samuel Pottinger <sam@gleap.org>
2024-11-26 22:58:01 +0000

    Make large cluster for all preprocess.

--- run.sh
+++ run.sh
@@ -1,4 +1,5 @@
 mkdir workspace
 
 python3 -m luigi --module breakpoint_tasks RunThroughPreprocessTask --local-scheduler --workers 8
+python3 -m luigi --module breakpoint_tasks RunThroughPreprocessFutureTask --local-scheduler --workers 8
 python3 -m luigi --module breakpoint_tasks ExecuteAllWithCluster --local-scheduler --workers 4
