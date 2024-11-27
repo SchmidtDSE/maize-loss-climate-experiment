41147128a45547c806a86ef690a18d5f1f73e2a9
A Samuel Pottinger <sam@gleap.org>
2024-11-27 00:21:40 +0000

    Remove cluster from usda tasks.

--- usda_tasks.py
+++ usda_tasks.py
@@ -6,9 +6,12 @@ are highly summarized.
 License: BSD
 """
 import csv
+import io
 import itertools
+import zipfile
 
 import luigi
+import requests
 import toolz.itertoolz
 
 import cluster_tasks
@@ -50,96 +53,8 @@ FIELDS = [
 FINAL_FIELDS = ['year', 'actualCount', 'actualClaims', 'actualLossRatio', 'simCount', 'simClaims']
 
 
-def process_year(year):
-    """Summarize a year of summary of business records.
-
-    Args:
-        year: The integer year to process.
-    Returns:
-
-    """
-    import csv
-    import io
-    import zipfile
-
-    import toolz.itertoolz
-    import requests
-
-    url = ZIP_URL_TEMPLATE % year
-    response = requests.get(url)
-
-    assert response.status_code < 400
-
-    zip_data = io.BytesIO(response.content)
-
-    def make_row(fields):
-        fields_stripped = map(lambda x: x.strip(), fields)
-        fields_zipped = zip(FIELDS, fields_stripped)
-        return dict(fields_zipped)
-
-    def simplify_row(raw_row):
-        return {
-            'year': year,
-            'county': '%s, %s' (raw_row['countyName'], raw_row['stateAbbrevation']),
-            'count': int(raw_row['unitsPremiumCount']),
-            'indemnified': int(raw_row['unitsIndemnifiedCount']),
-            'lossRatio': float(raw_row['lossRatio']),
-            'program': raw_row['insuranceName']
-        }
-
-    def combine_rows(row_a, row_b):
-        assert row_a['year'] == row_b['year']
-        assert row_a['county'] == row_b['county']
-        assert row_a['program'] == row_b['program']
-
-        combined_count = row_a['count'] + row_b['count']
-        combined_indemnified = row_a['indemnified'] + row_b['indemnified']
-
-        loss_ratio_weighted_a = row_a['count'] * row_a['lossRatio']
-        loss_ratio_weighted_b = row_b['count'] * row_b['lossRatio']
-        loss_ratio = (loss_ratio_weighted_a + loss_ratio_weighted_b) / combined_count
-
-        return {
-            'year': row_a['year'],
-            'county': row_a['county'],
-            'program': row_a['program'],
-            'count': combined_count,
-            'indemnified': combined_indemnified,
-            'lossRatio': loss_ratio
-        }
-
-    with zipfile.ZipFile(zip_data) as zip_file:
-        csv_filename = zip_file.namelist()[0]
-
-        with zip_file.open(csv_filename) as csv_file:
-            csv_text = io.StringIO(csv_file.read().decode('utf-8'))
-            reader = csv.reader(csv_text, delimiter='|')
-
-            rows = map(make_row, reader)
-            maize_rows = filter(lambda x: x['commodityName'] == 'Corn')
-            in_scope_rows = filter(lambda x: x['insuranceName'] in ['YP', 'RP'], maize_rows)
-            valid_rows = filter(lambda x: x['count'] > 0, in_scope_rows)
-            simplified_rows = map(simplify_row, valid_rows)
-
-            reduced_rows_keyed = toolz.itertoolz.reduceby(
-                lambda x: x['county'] + '\t' + x['program'],
-                combine_rows,
-                simplified_rows
-            )
-            reduced_rows = reduced_rows_keyed.values()
-            return reduced_rows
-
-
 class SummarizeUsdaYearCountyTask(luigi.Task):
     """Summarize at the county level per year per program."""
-
-    def requires(self):
-        """Indicate that the cluster is required.
-
-        Returns:
-            cluster_tasks.StartClusterTask
-        """
-        return cluster_tasks.StartClusterTask()
 
     def output(self):
         """Indicate where the county per year per program output should be written.
@@ -155,7 +70,7 @@ class SummarizeUsdaYearCountyTask(luigi.Task):
         cluster.adapt(minimum=10, maximum=20)
         client = cluster.get_client()
 
-        future_nested = client.map(process_year, const.YEARS)
+        future_nested = client.map(lambda x: self._process_year(x), const.YEARS)
         nested = map(lambda x: x.result(), future_nested)
         flattened = itertools.chain(*nested)
 
@@ -163,6 +78,78 @@ class SummarizeUsdaYearCountyTask(luigi.Task):
             writer = csv.DictWriter()
             writer.writeheader(['year', 'county', 'program', 'count', 'indemnified', 'lossRatio'])
             writer.writerows(flattened)
+    
+    def process_year(self, year):
+        """Summarize a year of summary of business records.
+
+        Args:
+            year: The integer year to process.
+        Returns:
+            Summary rows.
+        """
+        url = ZIP_URL_TEMPLATE % year
+        response = requests.get(url)
+
+        assert response.status_code < 400
+
+        zip_data = io.BytesIO(response.content)
+
+        def make_row(fields):
+            fields_stripped = map(lambda x: x.strip(), fields)
+            fields_zipped = zip(FIELDS, fields_stripped)
+            return dict(fields_zipped)
+
+        def simplify_row(raw_row):
+            return {
+                'year': year,
+                'county': '%s, %s' (raw_row['countyName'], raw_row['stateAbbrevation']),
+                'count': int(raw_row['unitsPremiumCount']),
+                'indemnified': int(raw_row['unitsIndemnifiedCount']),
+                'lossRatio': float(raw_row['lossRatio']),
+                'program': raw_row['insuranceName']
+            }
+
+        def combine_rows(row_a, row_b):
+            assert row_a['year'] == row_b['year']
+            assert row_a['county'] == row_b['county']
+            assert row_a['program'] == row_b['program']
+
+            combined_count = row_a['count'] + row_b['count']
+            combined_indemnified = row_a['indemnified'] + row_b['indemnified']
+
+            loss_ratio_weighted_a = row_a['count'] * row_a['lossRatio']
+            loss_ratio_weighted_b = row_b['count'] * row_b['lossRatio']
+            loss_ratio = (loss_ratio_weighted_a + loss_ratio_weighted_b) / combined_count
+
+            return {
+                'year': row_a['year'],
+                'county': row_a['county'],
+                'program': row_a['program'],
+                'count': combined_count,
+                'indemnified': combined_indemnified,
+                'lossRatio': loss_ratio
+            }
+
+        with zipfile.ZipFile(zip_data) as zip_file:
+            csv_filename = zip_file.namelist()[0]
+
+            with zip_file.open(csv_filename) as csv_file:
+                csv_text = io.StringIO(csv_file.read().decode('utf-8'))
+                reader = csv.reader(csv_text, delimiter='|')
+
+                rows = map(make_row, reader)
+                maize_rows = filter(lambda x: x['commodityName'] == 'Corn')
+                in_scope_rows = filter(lambda x: x['insuranceName'] in ['YP', 'RP'], maize_rows)
+                valid_rows = filter(lambda x: x['count'] > 0, in_scope_rows)
+                simplified_rows = map(simplify_row, valid_rows)
+
+                reduced_rows_keyed = toolz.itertoolz.reduceby(
+                    lambda x: x['county'] + '\t' + x['program'],
+                    combine_rows,
+                    simplified_rows
+                )
+                reduced_rows = reduced_rows_keyed.values()
+                return reduced_rows
 
 
 class SummarizeYearlySimClaims(luigi.Task):
