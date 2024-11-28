74182e5bb02a2a379a5aae3c510939cd7997a3a7
A Samuel Pottinger <sam@gleap.org>
2024-11-28 16:21:04 +0000

    Add yield delta to clean.

--- clean.sh
+++ clean.sh
@@ -7,6 +7,7 @@ mv ./workspace/climate_historic.csv ./clean_hold
 mv ./workspace/climate_2030_SSP245.csv ./clean_hold
 mv ./workspace/climate_2050_SSP245.csv ./clean_hold
 mv ./workspace/yield.csv ./clean_hold
+mv ./workspace/yield_delta.csv ./clean_hold
 mv ./workspace/yield_beta.csv ./clean_hold
 mv ./workspace/unit_sizes_2023.csv ./clean_hold
 mv ./workspace/usda_post_summary.csv ./clean_hold
