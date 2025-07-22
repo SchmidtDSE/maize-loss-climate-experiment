a9127e141f7cf4a756f19a626341c52e365e6499
Sam Pottinger <sam.pottinger@berkeley.edu>
2025-07-22 21:36:49 +0000

    Update deploy to include supporting files.
    Update deploy to include supporting files with exception of font which
    can be quite large / slow.

--- paper/viz/script/prepare_deploy.sh
+++ paper/viz/script/prepare_deploy.sh
@@ -8,12 +8,18 @@ cp *.js deploy
 cp *.html deploy
 # cp *.md deploy
 cp *.txt deploy
+cp -r img deploy
+cp -r data deploy
+# cp -r font deploy  # Commented out - font directory is quite large
+cp -r third_party deploy
 
 python3 script/rename_py_files.py deploy
 python3 script/update_py_paths.py deploy/index.html deploy/index.html
 python3 script/update_epoch.py deploy/index.html deploy/index.html
 python3 script/update_py_paths.py deploy/workshop.html deploy/workshop.html
 python3 script/update_epoch.py deploy/workshop.html deploy/workshop.html
+python3 script/update_py_paths.py deploy/workshop_brief.html deploy/workshop_brief.html
+python3 script/update_epoch.py deploy/workshop_brief.html deploy/workshop_brief.html
 
 echo "== Prepared deploy directory. =="
 
